25bd4eee1f5b47328aa967f713722ec67a0d477b
Анна Грозеску <agy96@yandex.ru>
2022-07-17 20:00:52 +0300

    Change

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int TicketPrice = 5999;
-        int RublesForOneMili = 20;
+        int ticketPrice = 5999;
+        int rublesForOneMili = 20;
 
-        int bonus = TicketPrice / RublesForOneMili;
+        int bonus = ticketPrice / rublesForOneMili;
         System.out.println("Количество начисленных миль:" + bonus);
 
 

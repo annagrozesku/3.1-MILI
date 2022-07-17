2ae722eaf6f0d0346fc63a40504322b38ae1ab6c
Анна Грозеску <agy96@yandex.ru>
2022-07-17 14:21:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int TicketPrice = 5999;
+        int RubblesForOneMili = 20;
+
+        int bonus = TicketPrice / RubblesForOneMili;
+        System.out.println(bonus);
+
+
+    }
+}
